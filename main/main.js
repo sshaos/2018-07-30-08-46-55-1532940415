b7c273ba66e967160c459fbc9fee3e374484f8e9
shaoshuai <shaoshuai@hust.edu.cn>
2018-08-02 13:15:07 +0800

    change main

--- main/main.js
+++ main/main.js
@@ -1,15 +1,32 @@
-module.exports = function main(str) {
-  //字符串转化为数组
-  const arr = str;
-  arr.split("");
+//1.输入空字符串结果为0
+//2.字符串里面有非数字的情况报错
+//3.字符串里面有负号的运算“-123，2-13，5-4-”
+module.exports = function main(arr) {
   var sum = 0 ;
   for(var i = 0;i<arr.length;i++)
   {
+    if(arr[i]=='-')//字符串里面有负号
+    {
+      i++;
+      if(i == arr.length)//负号是否在字符串末尾
+      {
+        console.error('Please check your input!\n');
+        return;
+      }
+      if(isNaN(arr[i]))//判断当前位是否是数字，否则报错
+      {
+        console.error('Please check your input!\n')
+        return;
+      }
+      sum -= Number(arr[i]);
+      continue;
+    }
+    if(isNaN(arr[i]))//判断当前位是否是数字，否则报错
+    {
+      console.error('Please check your input!\n')
+      return;
+    }
     sum += Number(arr[i]);
   }
- 
-  //arr.map((a)=> Number(a));map遍历方式
-  //var sum = Number(arr[0])+Number(arr[1])+Number(arr[2]);
-  //const sum = arr.reduce((a,x) => a += x,0);
   return sum;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -5,6 +5,14 @@ describe('main()', () => {
     it('should calculate the sum', () => {
       expect(main("123")).toBe(6);
       expect(main("102")).toBe(3);
+      expect(main("-102")).toBe(1);
+      expect(main("-2-11")).toBe(-2);
+      expect(main("")).toBe(0);
+      expect(main(" ")).toBe(0);
+      //以下几种错误输入情况
+      expect(main("-2-1-")).toBe(undefined);
+      expect(main("@23")).toBe(undefined);
+      expect(main("1-#3")).toBe(undefined);
     });
 
 });
